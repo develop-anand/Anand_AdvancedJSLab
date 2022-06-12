264c444d94b22c7ca0fb674d355373bde2664ae6
Anand Verma <developanand@protonmail.com>
2022-06-12 18:51:04 +0530

    Initial Commit

---
+++ Lab_Assignment_WeatherApp/Index.js
@@ -0,0 +1,56 @@
+const userInput = document.querySelector('.searchBox');
+userInput.addEventListener('keypress', function (event) {
+    // event key has the information about key
+    console.log('keypress');
+    console.log(userInput.value);
+
+    if (event.code === 'Enter') {
+        fetchWeatherdata(userInput.value);
+    }
+});
+
+const fetchWeatherdata = (city) => {
+    const apiKey = 'd8cdfe5030a6b24a7f9f3906060759f0'
+    const url = `https://api.openweathermap.org/data/2.5/find?q=${city}&appid=${apiKey}&units=metric`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            showWeather(data);
+        })
+        .catch(error => console.log(error.message));
+};
+
+const showWeather = (data) => {
+
+    document.querySelector('.city').textContent = data.list[0].name;
+    // document.querySelector('.city').textContent = data.list[0].name;
+    document.querySelector('.temperature').textContent = data.list[0].main.temp + ' °C';
+    document.querySelector('.weather').textContent = data.list[0].weather[0].main;
+    document.querySelector('.high_low').textContent = `${data.list[0].main.temp_max} °C /  ${data.list[0].main.temp_min} °C`;
+    document.querySelector('.date').textContent = getDateformat(data.list[0].dt);
+
+};
+
+const getDateformat = (dt) => {
+    
+    const date = new Date(dt * 1000);
+
+
+const days = [
+    'Sunday','Monday','Tuesday',
+    'Wednesday','Thursday','Friday','Saturday'
+]
+
+const months = [
+    'January','February','March',
+    'April','May','June',
+    'July','August','September',
+    'October','November','December'
+];
+
+const DateFormat = `${days[date.getDay()]} ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+    return formattedDate;
+}
+
